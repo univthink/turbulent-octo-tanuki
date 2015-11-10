479935f3fb3c87689657a8a04108276febebc3fe
Ben Barnett <benjamin@universallythinking.com>
2015-11-10 11:50:45 -0500

    production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
 //= require jquery_ujs
 //= require turbolinks
 //= require jquery
+//= require_tree .
